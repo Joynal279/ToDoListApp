79534676aaa23b12afff7cee74e5cdc8c81dabcf
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-10-25 19:46:57 +0600

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by JOYNAL ABEDIN on 25/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
